87287c0795411966e3ca444393345f877752c8df
HariOm Gaur <hogaur@HariOms-MacBook-Pro.local>
2015-06-19 22:25:29 +0530

    [Hari] escalation_rule controller spec fixes

--- spec/controllers/escalation_rules_controller_spec.rb
+++ spec/controllers/escalation_rules_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-
 RSpec.describe EscalationRulesController, :type => :controller do
   login_user
 
   let(:valid_attributes) {
-    {:name=>"Akash"}
+    {:name=>"Akash",
+     :organization=>(create :organization),
+  }
   }
 
   let(:invalid_attributes) {
@@ -17,7 +18,7 @@ RSpec.describe EscalationRulesController, :type => :controller do
   describe "GET index" do
     it "assigns all escalation_rules as @escalation_rules" do
       escalation_rule = EscalationRule.create! valid_attributes
-      escalation_rule.user=subject.current_user
+      escalation_rule.user_id=subject.current_user.id
       escalation_rule.save
       get :index, {}, valid_session
       expect(assigns(:escalation_rules)).to eq([escalation_rule])
@@ -50,18 +51,24 @@ RSpec.describe EscalationRulesController, :type => :controller do
   describe "POST create" do
     describe "with valid params" do
       it "creates a new EscalationRule" do
+        contact = subject.current_user.contacts.create
+        valid_attributes[:contacts] = Hash.new(contact)
         expect {
           post :create, {:escalation_rule => valid_attributes}, valid_session
         }.to change(EscalationRule, :count).by(1)
       end
 
       it "assigns a newly created escalation_rule as @escalation_rule" do
+        contact = subject.current_user.contacts.create
+        valid_attributes[:contacts] = Hash.new(contact)
         post :create, {:escalation_rule => valid_attributes}, valid_session
         expect(assigns(:escalation_rule)).to be_a(EscalationRule)
         expect(assigns(:escalation_rule)).to be_persisted
       end
 
       it "redirects to the created escalation_rule" do
+        contact = subject.current_user.contacts.create
+        valid_attributes[:contacts] = Hash.new(contact)
         post :create, {:escalation_rule => valid_attributes}, valid_session
         expect(response).to redirect_to(EscalationRule.last)
       end
@@ -72,7 +79,8 @@ RSpec.describe EscalationRulesController, :type => :controller do
   describe "PUT update" do
     describe "with valid params" do
       let(:new_attributes) {
-        { :name => 'sartaj'}
+        { :name => 'sartaj',
+          :contacts=>Hash.new(subject.current_user.contacts.create)}
       }
 
       it "updates the requested escalation_rule" do
@@ -85,12 +93,16 @@ RSpec.describe EscalationRulesController, :type => :controller do
 
       it "assigns the requested escalation_rule as @escalation_rule" do
         escalation_rule = EscalationRule.create! valid_attributes
+        contact = subject.current_user.contacts.create
+        valid_attributes[:contacts] = Hash.new(contact)
         put :update, {:id => escalation_rule.to_param, :escalation_rule => valid_attributes}, valid_session
         expect(assigns(:escalation_rule)).to eq(escalation_rule)
       end
 
       it "redirects to the escalation_rule" do
         escalation_rule = EscalationRule.create! valid_attributes
+        contact = subject.current_user.contacts.create
+        valid_attributes[:contacts] = Hash.new(contact)
         put :update, {:id => escalation_rule.to_param, :escalation_rule => valid_attributes}, valid_session
         expect(response).to redirect_to(escalation_rule)
       end
@@ -99,21 +111,13 @@ RSpec.describe EscalationRulesController, :type => :controller do
     describe "with invalid params" do
       it "assigns the escalation_rule as @escalation_rule" do
         @escalation_rule = EscalationRule.create! valid_attributes
-
-        @escalation_rule.user=subject.current_user
-        if @escalation_rule.save
-          put :update, {:id => @escalation_rule.to_param, :escalation_rule => invalid_attributes}, valid_session
-          expect(assigns(:escalation_rule)).to eq(@escalation_rule)
-        else
-          it "re-renders the 'edit' template" do
-            @escalation_rule = EscalationRule.create! valid_attributes
-            put :update, {:id => @escalation_rule.to_param, :escalation_rule => invalid_attributes}, valid_session
-            expect(response).to render_template("edit")
-          end
-        end
+        @escalation_rule.user_id=subject.current_user.id
+        put :update, {:id => @escalation_rule.to_param, :escalation_rule => invalid_attributes}, valid_session
+        expect(response).to render_template("edit")
       end
     end
   end
+
   describe "DELETE destroy" do
     it "destroys the requested escalation_rule" do
       escalation_rule = EscalationRule.create! valid_attributes
