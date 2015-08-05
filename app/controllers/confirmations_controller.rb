efca549a14550cfc60f276918b3d8c6fd9625d95
HariOm Gaur <hogaur@HariOms-MacBook-Pro.local>
2015-08-05 11:31:06 +0530

    [Hari] email verification link leads to signin page if linked more than once

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,13 +1,12 @@
 class ConfirmationsController < Devise::ConfirmationsController
-   def show
+  def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
-
     if resource.errors.empty?
       set_flash_message(:notice, :confirmed) if is_navigational_format?
       sign_in(resource_name, resource)
       respond_with_navigational(resource){ redirect_to confirmed_email_path }
     else
-      respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render_with_scope :new }
+      respond_with_navigational(resource.errors, :status => :unprocessable_entity){ redirect_to '/#signin'}
     end
   end
 
@@ -29,7 +28,7 @@ class ConfirmationsController < Devise::ConfirmationsController
     @unregistered_phone_number = current_user.contacts.select(&:unverified?).first
     if @unregistered_phone_number
       @unregistered_phone_number =  secure_phone_number @unregistered_phone_number.phone_number
-      
+
     end
   end
 
@@ -44,7 +43,7 @@ class ConfirmationsController < Devise::ConfirmationsController
   end
 
   def last_confirmed_phone_number
-     @phone_number = Contact.where(user: current_user).order(confirmed_at: :desc).first.phone_number
-     @phone_number = secure_phone_number @phone_number if @phone_number
+    @phone_number = Contact.where(user: current_user).order(confirmed_at: :desc).first.phone_number
+    @phone_number = secure_phone_number @phone_number if @phone_number
   end
 end
