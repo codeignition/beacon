a5b10810d0046894656f4f64c2c974a931ff2658
HariOm Gaur <hogaur@HariOms-MacBook-Pro.local>
2015-08-18 10:46:40 +0530

    [Hari] fixing migration file

--- db/migrate/20150814123001_add_airplane_mode_start_time_end_time_in_escalation_rules.rb
+++ db/migrate/20150814123001_add_airplane_mode_start_time_end_time_in_escalation_rules.rb
@@ -1,9 +1,5 @@
 class AddAirplaneModeStartTimeEndTimeInEscalationRules < ActiveRecord::Migration
   def change
-    remove_column :escalation_rules, :weekday_airplane_mode_start_time, :datetime
-    remove_column :escalation_rules, :weekday_airplane_mode_end_time, :datetime
-    remove_column :escalation_rules, :weekend_airplane_mode_start_time, :datetime
-    remove_column :escalation_rules, :weekend_airplane_mode_end_time, :datetime
     add_column :escalation_rules, :weekday_airplane_mode_start_time, :float
     add_column :escalation_rules, :weekday_airplane_mode_end_time, :float
     add_column :escalation_rules, :weekend_airplane_mode_start_time, :float
